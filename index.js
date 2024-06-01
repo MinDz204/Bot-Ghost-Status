d51c782983f1f91b65a020a58132fd6fded56e4d
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-01 23:50:55 +0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["LISTENING TO YUONGZMIN✨","PLAY MUSIC WITH YUONGZMIN✨"];
 
 
 let currentIndex = 0;
